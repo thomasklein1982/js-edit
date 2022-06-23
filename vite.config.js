8689d25071ebca90f51d06d7ff6a5064c83eaaff
Thomas Klein <thomas.klein.1982@gmail.com>
2022-06-23 21:44:58 +0200

    appjs update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue';
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+// primeicons müssen halb-automatisch hinzugefügt werden zu sw.js: 
+// primeicons.c9eaf535.eot primeicons.788dba0a.ttf primeicons.feb68bf6.woff primeicons.2ab98f70.svg
+// file:///C:/Users/Thomas%20Klein/Dropbox/Public/Webapps/vite-sw-inject/index.html
+// oder
+// https://thomaskl.uber.space/Webapps/vite-sw-inject/
 export default defineConfig({
   plugins: [
     vue(),
