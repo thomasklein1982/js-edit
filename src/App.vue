7f4fed10c5f6472920c0649a0efd8213e82ef0e5
Thomas Klein <thomas.klein.1982@gmail.com>
2023-02-14 23:24:43 +0100

    v75

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default{
     return {
       sourceCode: '',
       sourceCodeDebugging: '',
-      version: "73",
+      version: "75",
       breakpoints: [],
       paused: false,
       currentLine: -1
